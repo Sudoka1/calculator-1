5adcda33f4fa39b30ea813b147a83510e09fe5da
Sabir <sabir.suleyman@dci-student.org>
2024-04-16 14:30:48 +0200

    -2 korrektur

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const calc = new Calculator();
 console.log(calc.ratio(5, 7, 300)); // 420
 console.log(calc.percentage(3, 12)); // 25%
 console.log(calc.add(5, 7)); // 12
-console.log(calc.subtract(5, 7)); // -2
+console.log(calc.subtract(9, 7)); // -2
 console.log(calc.multiply(5, 7)); // 35
 console.log(calc.divide(35, 7)); // 5
 console.log(calc.remainder(7, 5)); // 2
